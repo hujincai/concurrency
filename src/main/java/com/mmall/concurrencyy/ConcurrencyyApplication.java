a384905728a9ed33adcbb6e712397a030b2472ab
hujincai <h18401632295@163.com>
2019-07-04 16:13:17 +0800

    concurrency

---
+++ src/main/java/com/mmall/concurrencyy/ConcurrencyyApplication.java
@@ -0,0 +1,13 @@
+package com.mmall.concurrencyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmall/concurrencyy/ConcurrencyyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mmall.concurrencyy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConcurrencyyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

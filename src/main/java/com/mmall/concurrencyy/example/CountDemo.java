1d98936bb50175e6ae0201f532ed6b7e04e320d6
hujincai <h18401632295@163.com>
2019-07-31 12:47:04 +0800

    count demo

--- src/main/java/com/mmall/concurrencyy/example/CountDemo.java
+++ src/main/java/com/mmall/concurrencyy/example/CountDemo.java
@@ -6,13 +6,4 @@ package com.mmall.concurrencyy.example;
  */
 public class CountDemo {
 
-    public static void main(String[] args) {
-        System.out.println(9996&9995);
-        // 10011100001111
-        //
-        // 10011100001110
-        // 10011100001101
-        // 10011100001100 9996
-        // 10011100001011 9995
-    }
 }
